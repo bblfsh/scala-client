2adf19e25c233553ea9aff4ce6af56d6ba012d5b
Alexander Bezzubov <bzz@apache.org>
2017-07-31 16:42:40 +0200

    Switch tests to Java driver

---
+++ src/test/resources/SampleJavaFile.java
@@ -0,0 +1,7 @@
+package org.sample.test;
+
+class SampleJavaFile {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
--- src/test/scala/org/bblfsh/client/BblfshClientTest.scala
+++ src/test/scala/org/bblfsh/client/BblfshClientTest.scala
@@ -2,22 +2,20 @@ package org.bblfsh.client
 
 import java.io.File
 
-import org.scalatest.{BeforeAndAfterAll, FunSuite}
+import org.scalatest.FunSuite
 
 import scala.io.Source
 
 
-class BblfshClientTest extends FunSuite with BeforeAndAfterAll {
+class BblfshClientTest extends FunSuite {
 
-  override def beforeAll() = {
+  //val filename = "src/test/resources/python_file.py" // driver fails with "Exception: Could not determine Python version"
 
-  }
-
-  test("Parse UAST for existing .py file") {
+  test("Parse UAST for existing .java file") {
     // given
     // Bblfsh server is running on port :9432
     val client = BblfshClient("0.0.0.0", 9432)
-    val filename = "src/test/resources/python_file.py" // client read it, and encode to utf-8
+    val filename = "src/test/resources/SampleJavaFile.java" // client read it, and encode to utf-8
     assert(new File(filename).exists())
     val fileContent = Source.fromFile(filename) .getLines.mkString
 
