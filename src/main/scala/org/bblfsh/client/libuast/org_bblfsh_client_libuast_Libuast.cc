508caf421fdc23466be952d6f2f7d8604992d434
Juanjo Alvarez <juanjo@sourced.tech>
2018-02-23 10:56:00 +0100

    Factorice filter functions init and exit. Fixes for the SBT build
    Signed-off-by: Juanjo Alvarez <juanjo@sourced.tech>

--- src/main/scala/org/bblfsh/client/libuast/org_bblfsh_client_libuast_Libuast.cc
+++ src/main/scala/org/bblfsh/client/libuast/org_bblfsh_client_libuast_Libuast.cc
@@ -143,20 +143,29 @@ JNIEXPORT jobject JNICALL Java_org_bblfsh_client_libuast_Libuast_filter
   return immList;
 }
 
+static const char *InitFilter(JNIEnv *env, jobject obj, jstring query) {
+  memTracker.EnterFilter();
+
+  const char *cstr = AsNativeStr(query);
+  if (env->ExceptionCheck() == JNI_TRUE || !cstr) {
+    throw std::runtime_error("");
+  }
+  return cstr;
+}
+
+static void FinishFilter(JNIEnv *env) {
+  memTracker.DisposeMem(env);
+  memTracker.ExitFilter();
+}
 
 JNIEXPORT jboolean JNICALL Java_org_bblfsh_client_libuast_Libuast_filterBool
   (JNIEnv *env, jobject self, jobject obj, jstring query) {
 
+  jobject *node = &obj;
   jboolean ret = false;
-  memTracker.EnterFilter();
 
   try {
-    jobject *node = &obj;
-    const char *cstr = AsNativeStr(query);
-    if (env->ExceptionCheck() == JNI_TRUE || !cstr) {
-      throw std::runtime_error("");
-    }
-
+    auto cstr = InitFilter(env, obj, query);
     bool ok;
     ret = (jboolean)UastFilterBool(ctx, node, cstr, &ok);
     if (!ok) {
@@ -165,24 +174,18 @@ JNIEXPORT jboolean JNICALL Java_org_bblfsh_client_libuast_Libuast_filterBool
     }
   } catch (std::runtime_error&) {}
 
-  memTracker.DisposeMem(env);
-  memTracker.ExitFilter();
+  FinishFilter(env);
   return ret;
 }
 
 JNIEXPORT jdouble JNICALL Java_org_bblfsh_client_libuast_Libuast_filterNumber
   (JNIEnv *env, jobject self, jobject obj, jstring query) {
 
+  jobject *node = &obj;
   jdouble ret = false;
-  memTracker.EnterFilter();
 
   try {
-    jobject *node = &obj;
-    const char *cstr = AsNativeStr(query);
-    if (env->ExceptionCheck() == JNI_TRUE || !cstr) {
-      throw std::runtime_error("");
-    }
-
+    auto cstr = InitFilter(env, obj, query);
     bool ok;
     ret = (jdouble)UastFilterNumber(ctx, node, cstr, &ok);
     if (!ok) {
@@ -191,24 +194,18 @@ JNIEXPORT jdouble JNICALL Java_org_bblfsh_client_libuast_Libuast_filterNumber
     }
   } catch (std::runtime_error&) {}
 
-  memTracker.DisposeMem(env);
-  memTracker.ExitFilter();
+  FinishFilter(env);
   return ret;
 }
 
 JNIEXPORT jstring JNICALL Java_org_bblfsh_client_libuast_Libuast_filterString
   (JNIEnv *env, jobject self, jobject obj, jstring query) {
 
+  jobject *node = &obj;
   const char *retStr = NULL;
-  memTracker.EnterFilter();
 
   try {
-    jobject *node = &obj;
-    const char *cstr = AsNativeStr(query);
-    if (env->ExceptionCheck() == JNI_TRUE || !cstr) {
-      throw std::runtime_error("");
-    }
-
+    auto cstr = InitFilter(env, obj, query);
     retStr = UastFilterString(ctx, node, cstr);
     if (retStr == NULL) {
       ThrowException(LastError());
@@ -216,8 +213,7 @@ JNIEXPORT jstring JNICALL Java_org_bblfsh_client_libuast_Libuast_filterString
     }
   } catch (std::runtime_error&) {}
 
-  memTracker.DisposeMem(env);
-  memTracker.ExitFilter();
+  FinishFilter(env);
   return env->NewStringUTF(retStr);
 }
 
