e5af8309e4f9a5212b7c3a108bca593952714e6a
Alexander Bezzubov <bzz@apache.org>
2019-08-14 20:01:16 +0200

    v2: expose different parse modes
    Signed-off-by: Alexander Bezzubov <bzz@apache.org>

--- src/test/scala/org/bblfsh/client/v2/BblfshClientParseTest.scala
+++ src/test/scala/org/bblfsh/client/v2/BblfshClientParseTest.scala
@@ -1,6 +1,9 @@
 package org.bblfsh.client.v2
 
 import java.nio.ByteBuffer
+
+import gopkg.in.bblfsh.sdk.v2.protocol.driver.Mode
+
 import scala.io.Source
 
 class BblfshClientParseTest extends BblfshClientBaseTest {
@@ -12,20 +15,48 @@ class BblfshClientParseTest extends BblfshClientBaseTest {
     assert(resp.errors.isEmpty)
   }
 
+  "Filtering UAST" should "work in Native mode" in {
+    val fileContent = Source.fromFile(fileName).getLines.mkString("\n")
+    val resp = client.parse(fileName, fileContent, Mode.NATIVE)
+    val node = resp.get
+
+    val iter = BblfshClient.filter(node, "//SimpleName")
+    iter.toList should have size (10) // number of Identifiers in the file
+    iter.close()
+  }
+
+  "Filtering UAST" should "work in Annotated mode" in {
+    val fileContent = Source.fromFile(fileName).getLines.mkString("\n")
+    val resp = client.parse(fileName, fileContent, Mode.ANNOTATED)
+    val node = resp.get
+
+    val iter = BblfshClient.filter(node, "//SimpleName[@role='Call']")
+    iter.toList should have size (1) // number of function called in the file
+    iter.close()
+  }
+
+  "Filtering UAST" should "work in Semantic mode" in {
+    val fileContent = Source.fromFile(fileName).getLines.mkString("\n")
+    val resp = client.parse(fileName, fileContent, Mode.SEMANTIC)
+    val node = resp.get
+
+    val iter = BblfshClient.filter(node, "//uast:Identifier[@role='Call']")
+    iter.toList should have size (1) // number of function called in the file
+    iter.close()
+  }
+
   "Decoded UAST after parsing" should "not be NULL" in {
     val uast = resp.uast.decode()
 
     assert(uast != null)
     assert(uast.nativeContext != 0)
 
-    println(uast)
     uast.dispose()
   }
 
   "Decoded UAST RootNode" should "not be NULL" in {
     val uast = resp.uast.decode()
     val rootNode: NodeExt = uast.root()
-    println(rootNode.getClass)
 
     rootNode should not be null
     rootNode.ctx should not be (0)
@@ -37,15 +68,11 @@ class BblfshClientParseTest extends BblfshClientBaseTest {
   "Encoding UAST to the same ContextExt" should "produce the same bytes" in {
     val uastCtx: ContextExt = resp.uast.decode()
     val rootNode: NodeExt = uastCtx.root()
-    println(s"Root node: $rootNode")
 
     val encodedBytes: ByteBuffer = uastCtx.encode(rootNode)
 
     encodedBytes.capacity should be(resp.uast.asReadOnlyByteBuffer.capacity)
     encodedBytes shouldEqual resp.uast.asReadOnlyByteBuffer
-
-    println(resp.uast.asReadOnlyByteBuffer)
-    println(encodedBytes)
   }
 
   "Encoding java UAST to a new Context" should "produce the same bytes" in {
@@ -62,7 +89,6 @@ class BblfshClientParseTest extends BblfshClientBaseTest {
 
 
   "Encoding python UAST to a new Context" should "produce the same bytes" in {
-    val client = BblfshClient("localhost", 9432)
     val fileName = "src/test/resources/python_file.py"
     val fileContent = Source.fromFile(fileName).getLines.mkString("\n")
     val resp = client.parse(fileName, fileContent)
