9e6849dcfc916edc95e5acd3a369aad9450784cc
ncordon <nacho.cordon.castillo@gmail.com>
2019-09-27 13:35:48 +0200

    Addresses review comments
    Adds set -e to build.sh, hight level comments to build.sh and .travis.yml, help command to build.sh and help invocation if no arguments are passed to the script
    
    Signed-off-by: ncordon <nacho.cordon.castillo@gmail.com>

--- build.sh
+++ build.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# This script supports Linux, macOS and Windows (git bash)
+# It has several options, which are executed sequentially in the
+# order they are fed to it:
+
+# --clean: cleans SDK folder and libuast downloaded files
+# --get-dependencies: fetches SDK (which include RPC protocol files) and libuast and moves
+#                     them to appropriate directories for the compilations to succeed
+# --native: compiles the native code, with appropriate flags for each system
+# -all: compiles both the native code and the Scala code, in that order
+
 PROTO_DIR="src/main/proto"
 SDK_MAJOR="v3"
 SDK_VERSION="${SDK_MAJOR}.1.0"
@@ -44,32 +54,36 @@ function setOSEnv {
 }
 
 function cleanFiles {
+    set -e
     echo "[clean] Cleaning downloads and compilation files..."
+    rm -rf -- "${UNZIP_DIR}"
+    rm -rf -- "${BBLFSH_PROTO}"
+    rm -f  -- "libuast-bin.tar.gz"
     rm -rf src/main/resources/{lib,libuast}
-    rm -rf ${UNZIP_DIR}
-    rm -rf ${BBLFSH_PROTO}
     rm -rf libuast
-    rm -f libuast-bin.tar.gz
 }
 
 # Downloads and move SDK files to appropriate directory,
 # including protocol buffer files
 function getSDKFiles {
+    set -e
     echo "[sdk] Downloading and installing SDK${SDK_MAJOR} protocol buffer files..."
+
     TAR_FILE=${SDK_VERSION}.tar.gz
-    mkdir -p ${SDK_PROTO}/protocol && \
-    mkdir -p ${SDK_PROTO}/uast/role && \
-    curl -SL https://github.com/bblfsh/sdk/archive/${SDK_VERSION}.tar.gz -o ${TAR_FILE} && \
-    tar xzf ${TAR_FILE} && \
-    cp ${UNZIP_DIR}/protocol/driver.proto ${SDK_PROTO}/protocol/ && \
-    cp ${UNZIP_DIR}/uast/role/generated.proto ${SDK_PROTO}/uast/role  && \
+    mkdir -p ${SDK_PROTO}/protocol
+    mkdir -p ${SDK_PROTO}/uast/role
+    curl -SL https://github.com/bblfsh/sdk/archive/${SDK_VERSION}.tar.gz -o ${TAR_FILE}
+    tar xzf ${TAR_FILE}
+    cp ${UNZIP_DIR}/protocol/driver.proto ${SDK_PROTO}/protocol/
+    cp ${UNZIP_DIR}/uast/role/generated.proto ${SDK_PROTO}/uast/role
     rm -rf ${UNZIP_DIR} ${TAR_FILE}
 
-    echo "[sdk] Done unpacking SDK"
+    echo "[sdk] Done unpacking SDK" 1>&2
 }
 
 # Checks that the library has been downloaded correctly
 function checkLibuastDownload {
+    set -e
     find src/main/resources
     nm src/main/resources/libuast/libuast${LIBUAST_FMT} | grep -c UastDecode > /dev/null || exit -2
     nm src/main/resources/libuast/libuast${LIBUAST_FMT} | wc -l > /dev/null || exit -2
@@ -77,47 +91,53 @@ function checkLibuastDownload {
 
 # Downloads and moves libuast to appropriate directory
 function getLibuast {
-    echo "[libuast] Downloading libuast binary"
+    set -e
+    echo "[libuast] Downloading libuast binary" 1>&2
 
      GH_URL="https://github.com/bblfsh/libuast"
      BINARY_RELEASE_URL="${GH_URL}/releases/download/v${LIBUAST_VERSION}/libuast-${OS}-amd64.tar.gz"
      TAR_FILE=libuast-bin.tar.gz
 
-     curl -sL ${BINARY_RELEASE_URL} -o ${TAR_FILE} && \
-     tar xzf libuast-bin.tar.gz && \
-     mv ${OS}-amd64 libuast && \
-     mkdir -p src/main/resources && \
-     mv libuast src/main/resources && \
-     rm -f ${TAR_FILE} && \
+     curl -sL ${BINARY_RELEASE_URL} -o ${TAR_FILE}
+     tar xzf libuast-bin.tar.gz
+     mv ${OS}-amd64 libuast
+     mkdir -p src/main/resources
+     mv libuast src/main/resources
+     rm -f ${TAR_FILE}
      checkLibuastDownload
-     echo "[libuast] Done downloading libuast"
+     echo "[libuast] Done downloading libuast" 1>&2
 }
 
 # Compiles the JNI part
 function compileNativeCode {
-    echo "[native-code] Compiling libuast bindings..."
+    set -e
+    echo "[native-code] Compiling libuast bindings..." 1>&2
 
     OUT_FOLDER=src/main/resources/lib/
     SRC_FOLDER="src/main/native"
     SRC_FILES="${SRC_FOLDER}/org_bblfsh_client_v2_libuast_Libuast.cc ${SRC_FOLDER}/jni_utils.cc"
 
-    mkdir -p ${OUT_FOLDER} && \
+    mkdir -p ${OUT_FOLDER}
     ${COMPILER} ${FLAGS} \
         -I/usr/include -I"${JAVA_HOME}/include/" -I"${OS_HEADERS}"  \
         -Isrc/main/resources/libuast \
         -o ${OUT_FOLDER}/libscalauast${LIBSCALAUAST_FMT} \
         ${SRC_FILES} \
-        src/main/resources/libuast/libuast${LIBUAST_FMT} && \
+        src/main/resources/libuast/libuast${LIBUAST_FMT}
     find ${OUT_FOLDER}
 
-    echo "[native-code] Done compiling libuast bindings..."
+    echo "[native-code] Done compiling libuast bindings..." 1>&2
 }
 
 function compileScalaCode {
-    echo "[scala-code] Compiling library uber .jar"
+    set -e
+    echo "[scala-code] Compiling library uber .jar" 1>&2
     ./sbt assembly
-    echo "[scala-code] Done compiling!"
+    echo "[scala-code] Done compiling!" 1>&2
 }
+
+# Make commands fail-fast
+set -e
 
 # Correctly set compilation environment for host OS
 setOSEnv
@@ -128,6 +148,10 @@ function usage() {
     echo "Usage: $0 [--clean|--get-dependencies|--native|--all]"
     exit -3
 }
+
+if [ "$#" -eq 0 ]; then
+    usage && exit -4
+fi
 
 for arg in "$@"; do
     case $arg in
@@ -145,8 +169,11 @@ for arg in "$@"; do
             compileNativeCode && \
             compileScalaCode
             ;;
+        "--help")
+            usage
+            ;;
         *)
-            echo "Wrong argument: $arg"
+            echo "Invalid argument: $arg" 1>&2
             usage
             ;;
     esac
